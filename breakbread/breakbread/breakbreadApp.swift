c8b503e8efd9d9fdd09b40729478d40559e32acb
mylo <mylo@Mac-5110.lan>
2026-04-05 23:40:04 -0700

    break ground

---
+++ breakbread/breakbread/breakbreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakbreadApp.swift
+//  breakbread
+//
+//  Created by mylo on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct breakbreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
